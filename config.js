9fa8a92c5e944563da0efda0daf55d6e0e9ec52f
nelsontky <nelson@ccb.wtf>
2020-11-13 23:16:48 +0800

    Updates

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 
-// Format: https://api.github.com//repos/{owner}/{repo}/issues/
+// Format: https://api.github.com/repos/{owner}/{repo}/issues/
 var GITHUB_ISSUES_LINK = "https://api.github.com/repos/nelsontky/gh-pages-url-shortener-db/issues/";
-var PATH_SEGMENTS_TO_SKIP = 1;
+var PATH_SEGMENTS_TO_SKIP = 0;
