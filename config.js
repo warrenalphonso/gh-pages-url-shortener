ef5955c7bd92983913fd451b8d93c5cadf574cfc
Warren Alphonso <warrenalphonso02@gmail.com>
2020-11-17 00:45:12 -0800

    Updates

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
-var GITHUB_ISSUES_LINK = "https://api.github.com/repos/warrenalphonso/gh-pages-url-shortener-db/issues/";
+var GITHUB_ISSUES_LINK = "https://api.github.com/repos/warrenalphonso/gh-pages-url-shortener/issues/";
 var PATH_SEGMENTS_TO_SKIP = 1;
 var HOST = "warrenalphonso.github.io";
